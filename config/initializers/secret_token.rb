852c4d49fb91c104739f0776e6692bfe97619179
unknown <Jim@.(none)>
2010-08-01 19:43:54 -0500

    initial demo commit
    learning process for ruby on rails
    first project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '0a26d8b935ef0cbd5665634067248e5a7b3104ec61aa7ebf3b66eb3ba2d37f80ab9d853949446dbe48e6f71ecbe43a42791b3fd8a5977b0ae0a4fec4e985de26'
